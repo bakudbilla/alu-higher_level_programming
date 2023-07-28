1b69f902436eea474f01cf0b9d564048000edbc1
Bernice Akudbilla <b.akudbilla@alustudent.com>
2023-07-28 08:50:40 -0700

    defining an empty square class

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -23,6 +23,6 @@ class Square:
             raise TypeError("size must be an integer")
         else:
             if size < 0:
-                raise ValueError("size must be >=0")
+                raise ValueError("size must be >= 0")
             else:
                 self.__size = size

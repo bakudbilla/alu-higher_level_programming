c4830d7374009412f8f1c6c1a629520723bd1e69
Bernice Akudbilla <b.akudbilla@alustudent.com>
2023-08-03 21:16:36 +0000

    SQL - More queries

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- lists all privileges of MySQL users user_0d_1 and user_0d_2
--- privileges are listed on localhost server
--- lis all privileges of the MySQL users
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- Lists all privileges of Mysql users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

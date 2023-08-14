5f144d1b61556658b971e20239a4a5e0e17854db
Bernice Akudbilla <b.akudbilla@alustudent.com>
2023-08-14 22:30:24 +0000

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,70 +1,69 @@
 #!/usr/bin/python3
-"""defines Square class that inherits from Rectangle"""
+"""Write the class Square that inherits from Rectangle"""
 
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """class for Square instances with
-    attributes from Rectangle class"""
+    """
+    defines class Square; inherits from class Rectangle
+    Inherited Attributes:
+        id
+        __weight        __height
+        __x             __y
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """initializes Square instance"""
+        """Initialize"""
         super().__init__(size, size, x, y, id)
         self.size = size
 
     @property
     def size(self):
-        """gets private instance attribute size"""
-        return (self.width)
+        """Getter size"""
+        return self.width
 
     @size.setter
     def size(self, value):
-        """sets private instance attribute size and
-        uses to set width and height"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width must be > 0")
-        else:
-            self.width = value
-            self.height = value
+        """Setter size - sets width and height as size"""
+        self.width = value
+        self.height = value
 
     def __str__(self):
-        """override __str__ with new string in the format
-        [Square] (<id>) <x>/<y> - <size>"""
-        str_rep = "[Square] ({}) {}/{} - {}".format(
-            str(self.id), str(self.x), str(self.y), str(self.width))
-        return (str_rep)
+        """Prints [Square] (<id>)"""
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
+            self.__class__.__name__, self.id, self.x, self.y,
+            self.size)
 
     def update(self, *args, **kwargs):
-        """assigns an argument to each attribute of Square"""
-        if args and len(args) != 0:
-            for i in range(len(args)):
-                if i == 0:
-                    self.id = args[i]
-                if i == 1:
-                    self.size = args[i]
-                if i == 2:
-                    self.x = args[i]
-                if i == 3:
-                    self.y = args[i]
+        """
+        If args: set attributes in this order: id, width, height
+        """
+        if args:
+            for k, v in enumerate(args):
+                if k == 0:
+                    self.id = v
+                elif k == 1:
+                    self.size = v
+                elif k == 2:
+                    self.x = v
+                else:
+                    self.y = v
         else:
-            for kw in kwargs:
-                if kw == "id":
-                    self.id = (kwargs[kw])
-                if kw == "size":
-                    self.size = (kwargs[kw])
-                if kw == "x":
-                    self.x = (kwargs[kw])
-                if kw == "y":
-                    self.y = (kwargs[kw])
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
 
     def to_dictionary(self):
-        """returns dictionary representation of a Square"""
-        dict_rep = {}
-        dict_rep["id"] = self.id
-        dict_rep["size"] = self.size
-        dict_rep["x"] = self.x
-        dict_rep["y"] = self.y
-        return dict_rep
+        """Return dictionary representation"""
+        d = {}
+        d["id"] = self.id
+        d["size"] = self.size
+        d["x"] = self.x
+        d["y"] = self.y
+        return d

b8094a4a0a503425451e5f9a6baa2cddb51989df
Bernice Akudbilla <b.akudbilla@alustudent.com>
2023-08-14 12:49:32 -0700

    task5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""
-This is the "5-test_indentation" module.
-The 5-text_indentation module supplies one function, text_indentation(text).
-"""
+"""defines function to print text with 2 newlines after '.' '?' or ':' chars"""
 
 
 def text_indentation(text):
-    """splits a text into lines along "?", ":", "." followed by 2 new lines"""
+    """prints text with 2 newlines after '.' '?' or ':' chars"""
     if type(text) is not str:
         raise TypeError("text must be a string")
-    flag = 0
-    for a in text:
-        if flag == 0:
-            if a == ' ':
-                continue
-            else:
-                flag = 1
-        if flag == 1:
-            if a == '?' or a == '.' or a == ':':
-                print(a)
-                print()
-                flag = 0
-            else:
-                print(a, end="")
+    previous = ""
+    for char in text:
+        # leading whitespace
+        if char is " " and char is text[0] and previous is "":
+            previous = "\n"
+            continue
+        # whitespaces after newline
+        if char is " " and previous is "\n":
+            continue
+        # matching character, print char, print newlines
+        if char is "." or char is "?" or char is ":":
+            print(char)
+            print()
+            previous = "\n"
+        else:
+            print(char, end="")
+            previous = char

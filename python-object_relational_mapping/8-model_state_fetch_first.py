0c547657bda5701f4a5824e306b8a7c3f015bbfd
Bernice Akudbilla <b.akudbilla@alustudent.com>
2023-08-15 01:43:15 -0700

    task 8&10

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,6 +3,7 @@
 This prints first State object from the database without fetch all states
 using SQLAlchemy and importing State and Base from model_state
 """
+
 from sys import argv
 from model_state import Base, State
 
@@ -10,6 +11,7 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+        #Engine database
         engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
             argv[1], argv[2], argv[3]), pool_pre_ping=True)
         Base.metadata.create_all(engine)

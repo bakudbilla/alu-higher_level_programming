040f586c94868eb1a71cac83b9cbaada17a959d1
Bernice Akudbilla <b.akudbilla@alustudent.com>
2023-08-15 04:26:12 -0700

    task 11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
 """
-adds 'Louisiana' object to the database
-using SQLALCHEMY and session maker
+update state: given id, change state name
+parameters given to script: username, password, database
 """
+
 from sys import argv
 from model_state import Base, State
-
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-            argv[1], argv[2], argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        session.add(State(name='Louisiana'))
-        session.commit()
-        for instance in session.query(State).filter_by(name='Louisiana'):
-                print(instance.id)
-        session.close()
+
+    # make engine for database
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # add new state and commit to table
+    new = State(name="Louisiana")
+    session.add(new)
+    session.commit()
+
+    print("{:d}".format(new.id))
+
+    session.close()
